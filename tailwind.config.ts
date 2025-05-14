96e6dd5f7baa4984b19f6b1f864107e032ccbe5c
Stasshe <egnm9stasshe@gmail.com>
2025-05-14 09:28:14 +0000

    fix: Update Tailwind CSS configuration and handle unknown utility class error

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,6 @@
 import type { Config } from 'tailwindcss';
-import { preset } from '@tailwindcss/postcss';
 
 const config: Config = {
-  presets: [preset],
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
