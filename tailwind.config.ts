b4563b6b80e122e413c9331c6076c472d2baeee3
Stasshe <egnm9stasshe@gmail.com>
2025-05-14 09:20:44 +0000

    feat: Add PostCSS preset to Tailwind CSS configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,8 @@
 import type { Config } from 'tailwindcss';
+import { preset } from '@tailwindcss/postcss';
 
 const config: Config = {
+  presets: [preset],
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
